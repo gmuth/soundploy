660d07214f191603d65a4221d92d341ff84f5d35
gmuth <gerhard.muth@gmx.de>
2026-05-12 14:38:45 +0200

    add services and station.php

---
+++ v2/orion/station.php
@@ -0,0 +1,23 @@
+<?php
+
+// This script supports the Bose mobile app use case "play custom stream"
+// https://content.api.bose.io/core02/svc-bmx-adapter-orion/prod/orion/station?data=
+
+// Get url parameter data which is a base64 encoded json
+$data = json_decode(base64_decode($_GET["data"]), true);
+
+// Map to Bose compatible BMX response
+$response = [
+    "audio" => [
+        "isRealtime" => true,
+        "hasPlaylist" => false,
+        "streamUrl" => $data["streamUrl"] ?? ""
+    ],
+    "imageUrl" => $data["imageUrl"] ?? "",
+    "name" => $data["name"] ?? "",
+    "streamType" => "liveRadio"
+];
+
+// Output as json
+header("Content-Type: application/json");
+echo json_encode($response, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
